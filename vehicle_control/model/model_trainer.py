cddae805d8615a612d4b60322591dfb061fc2df5
Ken Power <ken.power@gmail.com>
2021-07-04 20:12:26 +0100

    Batch image generator needs an image augmentor

--- vehicle_control/model/model_trainer.py
+++ vehicle_control/model/model_trainer.py
@@ -7,18 +7,19 @@ class ModelTrainer:
     def __init__(self, model):
         self.model = model
 
-        self.epochs = 10
-        self.batch_size = 100
+        self.epochs = 5
+        self.batch_size = 150
         self.steps_per_epoch = 300  # len(X_train)//batch_size
         self.validation_steps = 200  # len(X_valid)//batch_size
 
-        self.batch_generator = BatchImageGenerator()
+        image_augmentor = ImageAugmentor()
+        self.image_generator = BatchImageGenerator(image_augmentor)
 
 
     def train_model(self, X_train, y_train, X_valid, y_valid):
 
-        training_generator = self.batch_generator(X_train, y_train, self.batch_size, True)
-        validation_generator = self.batch_generator(X_valid, y_valid, self.batch_size, False)
+        training_generator = self.image_generator.batch_generator(X_train, y_train, self.batch_size, True)
+        validation_generator = self.image_generator.batch_generator(X_valid, y_valid, self.batch_size, False)
 
         history = self.model.fit(training_generator,
                                  steps_per_epoch=self.steps_per_epoch,
